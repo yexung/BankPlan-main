fe2e75a3b9fe9a51060cf2c80c7d78d5520f3b74
YESUNG <yesung012929@naver.com>
2024-04-22 10:28:59 +0900

    환율

---
+++ BankPlan/ExchangeRateVC.swift
@@ -0,0 +1,29 @@
+//
+//  ExchangeRateVC.swift
+//  BankPlan
+//
+//  Created by 윤예성 on 4/22/24.
+//
+
+import UIKit
+
+class ExchangeRateVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
