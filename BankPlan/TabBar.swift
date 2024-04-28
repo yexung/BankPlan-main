1b4fa7aefe14345af915b84173a90fff869745ae
YESUNG <yesung012929@naver.com>
2024-04-28 15:08:40 +0900

    중간점검

---
+++ BankPlan/TabBar.swift
@@ -0,0 +1,20 @@
+//
+//  TabBar.swift
+//  BankPlan
+//
+//  Created by 윤예성 on 4/28/24.
+//
+
+import UIKit
+
+class TabBar: UITabBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
