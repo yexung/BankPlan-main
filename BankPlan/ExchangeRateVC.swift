c18934baebeca401da98056ee2d750254c96d7c5
YESUNG <yesung012929@naver.com>
2024-05-21 01:39:36 +0900

    실시간 시간 성공

--- BankPlan/ExchangeRateVC.swift
+++ BankPlan/ExchangeRateVC.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Foundation
+
 struct ExchangeInfo {
     let currencyCode: String
     let exchangeRate: Double
@@ -16,29 +18,24 @@ class ExchangeRateCell: UITableViewCell {
     @IBOutlet weak var CurrencyCodeText: UILabel!
     @IBOutlet weak var ExchangeRateText: UILabel!
     @IBOutlet weak var RiseRateText: UILabel!
-    
     @IBOutlet weak var CellView: UIView!
-    
-    
-override func awakeFromNib() {
-    
-//            super.awakeFromNib()
-//            // Set the corner radius for the cell's content view
-    CellView.layer.cornerRadius = 12
-    CellView.layer.masksToBounds = true
-//
-//            // Optionally, set a border to see the rounded corners clearly
-//            contentView.layer.borderWidth = 1.0
-//            contentView.layer.borderColor = UIColor.lightGray.cgColor
-//
-//            // Set the background color to clear
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        CellView.layer.cornerRadius = 12
+        CellView.layer.masksToBounds = true
+        // Optionally, set a border to see the rounded corners clearly
+//        contentView.layer.borderWidth = 1.0
+//        contentView.layer.borderColor = UIColor.lightGray.cgColor
+//        // Set the background color to clear
 //        backgroundColor = .yellow
-        }
+    }
 }
 
 class ExchangeRateVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var ExchangeRateTableView: UITableView!
     @IBOutlet weak var dismissButton: UIButton!
+    @IBOutlet weak var Time: UILabel!
 
     var ExchangeRateData: [ExchangeInfo] = [
         ExchangeInfo(currencyCode: "USD", exchangeRate: 1401.05, riseRate: 2.1),
@@ -46,12 +43,33 @@ class ExchangeRateVC: UIViewController, UITableViewDataSource, UITableViewDelega
         ExchangeInfo(currencyCode: "USD", exchangeRate: 1401.05, riseRate: 2.1)
     ]
     
+    var timer: Timer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         ExchangeRateTableView.delegate = self
         ExchangeRateTableView.dataSource = self
+
+        // Initialize and start the timer
+        startTimer()
     }
-    
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Invalidate the timer when the view is about to disappear
+        timer?.invalidate()
+    }
+
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimeLabel), userInfo: nil, repeats: true)
+    }
+
+    @objc func updateTimeLabel() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        Time.text = formatter.string(from: Date())
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return ExchangeRateData.count
     }
@@ -62,7 +80,7 @@ class ExchangeRateVC: UIViewController, UITableViewDataSource, UITableViewDelega
         }
         
         let ExchangeRate = ExchangeRateData[indexPath.row]
-        cell.CurrencyCodeText.text = "\(ExchangeRate.currencyCode)"
+        cell.CurrencyCodeText.text = ExchangeRate.currencyCode
         cell.ExchangeRateText.text = "\(ExchangeRate.exchangeRate)"
         cell.RiseRateText.text = "\(ExchangeRate.riseRate)%"
         
@@ -72,8 +90,8 @@ class ExchangeRateVC: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBAction func tapDismiss(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
+
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 78
     }
 }
-
