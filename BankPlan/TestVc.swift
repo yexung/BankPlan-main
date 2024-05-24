9c4d918a24c3a5de7ecb051698f5aebbcdd44e2e
YESUNG <yesung012929@naver.com>
2024-05-25 00:56:06 +0900

    asd

---
+++ BankPlan/TestVc.swift
@@ -0,0 +1,29 @@
+//
+//  TestVc.swift
+//  BankPlan
+//
+//  Created by 윤예성 on 5/24/24.
+//
+
+import UIKit
+
+class TestVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
